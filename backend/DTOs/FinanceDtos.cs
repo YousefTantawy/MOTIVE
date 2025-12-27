0a310231c2cc16daba54340b766d3619ac081080
Yousef Tantawy <Goe@DESKTOP-JDQISI7.localdomain>
2025-12-28 01:15:10 +0200

    Added APIs
    Added payement, enrollment, delete and update phone number

--- backend/DTOs/FinanceDtos.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MotiveBackend.DTOs
-{
-    public class ProcessPaymentDto
-    {
-        public ulong UserId { get; set; }
-        public ulong CourseId { get; set; }
-        public string PaymentMethod { get; set; } // e.g., "Visa", "PayPal"
-    }
-}
