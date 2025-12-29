5e48b999b86ea6ef8583295e80a02d9f5e787137
TheHoshii <amr.tarek.gamal.el.din@gmail.com>
2025-12-29 20:57:20 +0200

    part 2

--- frontend/src/pages/CertificatePage.tsx
+++ frontend/src/pages/CertificatePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../lib/axios";
-import { Button } from "../../components/ui/Button";
+import { Button } from "../components/ui/Button";
 
 interface CertificateResponse {
   studentName: string;
