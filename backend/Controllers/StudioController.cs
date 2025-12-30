24aa694ac67428a417c395d24007686d99484d0f
Yousef Tantawy <Goe@DESKTOP-JDQISI7.localdomain>
2025-12-30 02:15:00 +0200

    Finished 66% of Studio APIs

--- backend/Controllers/StudioController.cs
+++ backend/Controllers/StudioController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MotiveBackend.Data;
+using MotiveBackend.Models;
 
 namespace MotiveBackend.Controllers
 {
@@ -7,6 +10,47 @@ namespace MotiveBackend.Controllers
     [ApiController]
     public class StudioController : ControllerBase
     {
+        private readonly Ecen424DbProjectContext _context;
 
+        public StudioController(Ecen424DbProjectContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("courses/{userId}")]
+        public async Task<IActionResult> GetInstructorCourses(ulong userId)
+        {
+            var courses = await _context.Courses
+                .Where(c => c.InstructorId == userId)
+                .Select(c => new
+                {
+                    c.CourseId,
+                    c.Title,
+                    c.Price,
+                    c.Thumbnail,
+                    c.Category,
+                    StudentCount = _context.Enrollments.Count(e => e.CourseId == c.CourseId)
+                })
+                .ToListAsync();
+
+            return Ok(courses);
+        }
+
+        [HttpGet("revenue/{userId}")]
+        public async Task<IActionResult> GetTotalRevenue(ulong userId)
+        {
+            var totalRevenue = await _context.Enrollments
+                .Include(e => e.Course)
+                .Where(e => e.Course.InstructorId == userId)
+                .SumAsync(e => e.Course.Price);
+
+            return Ok(new { UserId = userId, TotalRevenue = totalRevenue });
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateCourse()
+        {
+            return Ok();
+        }
     }
 }
